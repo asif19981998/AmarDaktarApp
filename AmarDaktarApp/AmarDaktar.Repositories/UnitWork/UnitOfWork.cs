b2ba74060336964eef4eac429efe836d1a2ed360
asif19981998 <rakibulasif1998@gmail.com>
2021-04-17 16:16:12 +0600

    Unit of work Implementation

---
+++ AmarDaktarApp/AmarDaktar.Repositories/UnitWork/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿
+using AmarDaktar.DataBaseContext;
+using AmarDaktar.Repositories.Abastractions.IUnitWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmarDaktar.Repositories.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(AmarDaktarDbContext db)
+        {
+
+        }
+     
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            return null;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
